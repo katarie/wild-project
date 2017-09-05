d580e0e5fdd9a5a6890c26544eb64eda30cba6ab
katarie <valentin.veler2@gmail.com>
2017-09-05 11:11:04 +0200

    first test  github

---
+++ same_file.js
@@ -0,0 +1,2 @@
+Il est parfois plus simple de taper une commande que d'effectuer des manipulations demandant beaucoup de clics de souris dans une interface graphique.
+C'est aussi un moyen plus simple pour expliquer comment faire quelque chose à quelqu'un (sur un forum par exemple), puisque il suffit d'indiquer la commande et non la suite de clics à effectuer sur l'interface graphique.
